1de582f2451174052e4557d7a5be60c98d0c894e
CuityMiko <15201776956@163.com>
2017-05-30 22:09:35 +0800

    react项目骨架

---
+++ src/components/Hello.js
@@ -0,0 +1,16 @@
+
+import './Hello.css';
+import './Hello.scss'
+
+import React, {Component} from 'react'
+
+export default class Hello extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Hello React</h1>
+            </div>
+        )
+    }
+}
